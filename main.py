86e5c0aef81df4b052f26127013cf6b6edadd1a3
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-26 23:48:46 +0700

    Using Python break with for loop

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 if __name__ == "__main__":
-  print("Python break")
+  # Using Python break with for loop
+
+  """
+  for index in range(n):
+    # more code here
+    if condition:
+      break
+  """
+
+  for index in range(0, 10):
+    print(index)
+    if index == 3:
+      break
+
+  for x in range(5):
+    for y in range(10):
+      if y > 1:
+        break
+      print(f"({x}, {y})")
